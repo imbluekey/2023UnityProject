308c8c998ce19467965a4e85093b4287ab7d062c
Todom2 <schoolpang@naver.com>
2024-01-06 18:54:54 +0900

    merge test2
    m

--- Assets/Scripts/Test1.cs
+++ Assets/Scripts/Test1.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test1 : MonoBehaviour
+public class test1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Todom working?
     }
 
     // Update is called once per frame

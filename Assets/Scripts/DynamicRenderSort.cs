9b138d579e42b8b95685161e53913b5c707e5d6e
BlueKey <imbluekey@naver.com>
2024-01-29 21:25:02 +0900

    save on 0129
    동적 레이어 구분 및 hp바 위치 조정 스크립트 작성

---
+++ Assets/Scripts/DynamicRenderSort.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicRenderSort : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private Vector3 ObjectBottomPoint;
+    private Vector3 ObjectsBoundsDistance;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        ObjectsBoundsDistance = new Vector3(0f, spriteRenderer.bounds.extents.y, 0f);
+        //Debug.Log("Object Bounds Extents Distance : " + ObjectsBoundsDistance);
+    }
+
+    void Update()
+    {
+        ObjectBottomPoint = gameObject.transform.position - ObjectsBoundsDistance ;
+        Debug.Log("Bound bottom coordinate : " + ObjectBottomPoint);
+        spriteRenderer.sortingOrder = Mathf.CeilToInt(ObjectBottomPoint.y)*-1;
+        
+    }
+}
